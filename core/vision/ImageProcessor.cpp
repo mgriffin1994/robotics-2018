ff50520f3067824ffa0a6f3c6c2e7df00db1a19b
Justin Nguyen <2014justinnguyen@gmail.com>
2018-09-10 20:37:54 -0500

    Everything's broken

--- core/vision/ImageProcessor.cpp
+++ core/vision/ImageProcessor.cpp
@@ -124,30 +124,30 @@ void ImageProcessor::processFrame(){
   if(!color_segmenter_->classifyImage(color_table_)) return;
 
   std::vector<Region *> blobs;
-  findBlob(blobs);
-  detectBall(blobs);
-  detectGoal(blobs);
+//  findBlob(blobs);
+//  detectBall(blobs);
+//  detectGoal(blobs);
   beacon_detector_->findBeacons();
 
   // TODO: debug prints
-  for (int i = 0; i < blobs.size(); i++) {
-      Region blob = *blobs[i];
-      printf("==============================");
-      printf("blob.centerx: %d\n", blob.centerx);
-      printf("blob.centery: %d\n", blob.centery);
-      printf("blob.minx: %d\n", blob.minx);
-      printf("blob.miny: %d\n", blob.miny);
-      printf("blob.maxx: %d\n", blob.maxx);
-      printf("blob.maxy: %d\n", blob.maxy);
-      printf("blob.numRuns: %d\n", blob.numRuns);
-      printf("blob.blobSize: %d\n", blob.blobSize);
-      printf("blob.color: %d\n", blob.color);
-  }
+//  for (int i = 0; i < blobs.size(); i++) {
+//      Region blob = *blobs[i];
+//      printf("==============================");
+//      printf("blob.centerx: %d\n", blob.centerx);
+//      printf("blob.centery: %d\n", blob.centery);
+//      printf("blob.minx: %d\n", blob.minx);
+//      printf("blob.miny: %d\n", blob.miny);
+//      printf("blob.maxx: %d\n", blob.maxx);
+//      printf("blob.maxy: %d\n", blob.maxy);
+//      printf("blob.numRuns: %d\n", blob.numRuns);
+//      printf("blob.blobSize: %d\n", blob.blobSize);
+//      printf("blob.color: %d\n", blob.color);
+//  }
 
-  for(int i = 0; i < blobs.size(); i++) {
-      delete(blobs[i]);
-  }
-  blobs.clear();
+//  for(int i = 0; i < blobs.size(); i++) {
+//      delete(blobs[i]);
+//  }
+//  blobs.clear();
 }
 
 void ImageProcessor::detectGoal(std::vector<Region *> &blobs) {
@@ -228,10 +228,10 @@ void ImageProcessor::findBlob(std::vector<Region *>& blobs) {
     std::vector<std::vector<Run *>> all_runs;
 
     // Process from left to right
-    for(int x = 0; x < iparams_.width; x+=step) {
+    for(int y = 0; y < iparams_.height; y+=step) {
         // Process from top to bottom
         std::vector<Run *> hor_runs;
-        for(int y = 0; y < iparams_.height; y+=step) {
+        for(int x = 0; x < iparams_.width; x+=step) {
             // Retrieve the segmented color of the pixel at (x,y)
             uint8_t c = segImg [y * iparams_.width + x];
             int oldx, headx, headcol;
@@ -250,7 +250,7 @@ void ImageProcessor::findBlob(std::vector<Region *>& blobs) {
                 Run cur_run = *cur_run_ptr;
                 cur_run.start = headx;
                 cur_run.end = oldx;
-                cur_run.lead_parent = &cur_run;
+                cur_run.lead_parent = cur_run_ptr;
                 cur_run.color = oldcol;
                 cur_run.row = y;
                 cur_run.blobnum = -1;
@@ -260,10 +260,10 @@ void ImageProcessor::findBlob(std::vector<Region *>& blobs) {
 
                 if(y != 0) {
                     //look at all runs in previous row and find parents
-                    for(int i=0; i < all_runs[y-1].size(); i++){
-                        Run run = *all_runs[y-1][i];
+                    for(int i=0; i < all_runs[(y / step) - 1].size(); i++){
+                        Run run = *(all_runs[(y / step) - 1][i]);
                         if(run.color == cur_run.color && ((run.start >= cur_run.start && run.start <= cur_run.end) || (run.end >= cur_run.start && run.end <= cur_run.end))){
-                            if(cur_run.lead_parent == &cur_run){
+                            if(cur_run.lead_parent == cur_run_ptr){
                                 cur_run.lead_parent = run.lead_parent;
                             } else {
                                 cur_run.possible_parents.push_back(&run);
@@ -281,69 +281,71 @@ void ImageProcessor::findBlob(std::vector<Region *>& blobs) {
         }
         all_runs.push_back(hor_runs);
     }
-
-    //path compression
-    for(int i=0; i < all_runs.size(); i++){
-        for(int j=0; j < all_runs[i].size(); j++){
-            Run run = *all_runs[i][j];
-            if(run.possible_parents.size() > 0){
-                for(int k=0; k < run.possible_parents.size(); k++){
-                    Run parent = *run.possible_parents[k];
-                    if((*(parent.lead_parent)).lead_parent == parent.lead_parent){
-                        (*(parent.lead_parent)).lead_parent = run.lead_parent;
-                    }
-                }
-            }
-        }
-    }
+//
+//    //path compression
+//    for(int i=0; i < all_runs.size(); i++){
+//        for(int j=0; j < all_runs[i].size(); j++){
+//            Run run = *(all_runs[i][j]);
+//            if(run.possible_parents.size() > 0){
+//                for(int k=0; k < run.possible_parents.size(); k++){
+//                    Run parent = *(run.possible_parents[k]);
+//                    Run grand_parent = *(parent.lead_parent);
+//                    if(grand_parent.lead_parent == parent.lead_parent){
+//                        grand_parent.lead_parent = run.lead_parent;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
 
     //create blobs
-    int numblobs = 0;
-    for(int i=0; i < all_runs.size(); i++){
-        for(int j=0; j < all_runs[i].size(); j++){
-            Run run = *all_runs[i][j];
-            //create blobs whenever encouter root node or any of it's children
-            Run parent = *(run.lead_parent);
-            Run grand_parent = *(parent.lead_parent);
+//    int numblobs = 0;
+//    for(int i=0; i < all_runs.size(); i++){
+//        for(int j=0; j < all_runs[i].size(); j++){
+//            Run run = *all_runs[i][j];
+//            //create blobs whenever encouter root node or any of it's children
+//            Run parent = *(run.lead_parent);
+//            Run grand_parent = *(parent.lead_parent);
+//
+//            Region *blob_ptr = new Region();
+//            Region blob = *blob_ptr;
+//            if(grand_parent.blobnum == -1){
+//                blob.centerx = (run.start + run.end + 1) / 2;
+//                blob.centery = run.row;
+//                blob.minx = run.start;
+//                blob.miny = run.row;
+//                blob.maxx = run.end;
+//                blob.maxy = run.row;
+//                blob.numRuns = 1;
+//                blob.blobSize = (blob.maxx - blob.minx+1);
+//                blob.color = run.color;
+//                grand_parent.blobnum = numblobs++;
+//                blobs.push_back(blob_ptr);
+//            } else {
+//                //find correct relevant blob already existing and update it's values
+//                blob = *blobs[grand_parent.blobnum];
+//                blob.minx = ((blob.minx < run.start) ? blob.minx : run.start);
+//                blob.maxx = ((blob.maxx > run.end) ? blob.maxx : run.end);
+//                blob.miny = ((blob.miny < run.row) ? blob.minx : run.row);
+//                blob.maxy = ((blob.maxy > run.row) ? blob.minx : run.row);
+//                blob.centerx += (int) ((((run.start + run.end + 1) / 2) - blob.centerx) / blob.numRuns);
+//                blob.centery += (int) ((run.row - blob.centery + 1) / blob.numRuns);
+//                blob.numRuns++;
+//                blob.blobSize = (blob.maxx - blob.minx+1) * (blob.maxy - blob.miny+1);
+//            }
+//        }
+//    }
+//
+//    for(int i=0; i < all_runs.size(); i++){
+//        for(int j=0; j < all_runs[i].size(); j++){
+//            delete(all_runs[i][j]);
+//        }
+//        all_runs[i].clear();
+//    }
+//    all_runs.clear();
 
-            Region *blob_ptr = new Region();
-            Region blob = *blob_ptr;
-            if(grand_parent.blobnum == -1){
-                blob.centerx = (run.start + run.end + 1) / 2;
-                blob.centery = run.row;
-                blob.minx = run.start;
-                blob.miny = run.row;
-                blob.maxx = run.end;
-                blob.maxy = run.row;
-                blob.numRuns = 1;
-                blob.blobSize = (blob.maxx - blob.minx+1);
-                blob.color = run.color;
-                grand_parent.blobnum = numblobs++;
-                blobs.push_back(blob_ptr);
-            } else {
-                //find correct relevant blob already existing and update it's values
-                blob = *blobs[grand_parent.blobnum];
-                blob.minx = ((blob.minx < run.start) ? blob.minx : run.start);
-                blob.maxx = ((blob.maxx > run.end) ? blob.maxx : run.end);
-                blob.miny = ((blob.miny < run.row) ? blob.minx : run.row);
-                blob.maxy = ((blob.maxy > run.row) ? blob.minx : run.row);
-                blob.centerx += (int) ((((run.start + run.end + 1) / 2) - blob.centerx) / blob.numRuns);
-                blob.centery += (int) ((run.row - blob.centery + 1) / blob.numRuns);
-                blob.numRuns++;
-                blob.blobSize = (blob.maxx - blob.minx+1) * (blob.maxy - blob.miny+1);
-            }
-        }
-    }
-
-    for(int i=0; i < all_runs.size(); i++){
-        for(int j=0; j < all_runs[i].size(); j++){
-            delete(all_runs[i][j]);
-        }
-        all_runs[i].clear();
-    }
-    all_runs.clear();
-
-    std::sort(blobs.begin(), blobs.end(), compareBlobs);
+//    std::sort(blobs.begin(), blobs.end(), compareBlobs);
 }
 
 
