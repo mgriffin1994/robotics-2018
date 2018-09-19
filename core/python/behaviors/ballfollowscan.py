b76ccd514a160c7d0d11291d42b1d431e7fc95f1
Wally Guzman <gualbertoguzman@gmail.com>
2018-09-18 19:41:47 -0500

    ball follow scan looks pretty good

--- core/python/behaviors/ballfollowscan.py
+++ core/python/behaviors/ballfollowscan.py
@@ -16,7 +16,8 @@ from memory import joint_angles
 from task import Task
 
 
-time_delay = 2.0
+time_delay = 0.5
+ball_turn_delay = 0.1
 eps = 0.01
 
 class Ready(Task):
@@ -29,6 +30,27 @@ class Ready(Task):
             commands.setHeadPan(math.pi/3, time_delay)
             if self.getTime() > time_delay:
                self.finish()
+
+class TurnTowardBall(Node):
+    def run(self):
+        ball = mem_objects.world_objects[core.WO_BALL]
+        #commands.setStiffness()
+        if ball.seen:
+            commands.setHeadPan(ball.visionBearing, ball_turn_delay)
+            print("=======================================")
+            print(ball.visionBearing, ball.visionElevation)
+            print(ball.seen, ball.imageCenterX, ball.imageCenterY)
+            print("=======================================")
+        #else:
+        #    pan = core.joint_values[core.HeadPan]
+        #    if abs(pan - math.pi / 3) < eps: #if within eps from full right, turn left
+        #        commands.setHeadPan(-math.pi/3, time_delay)
+        #    elif abs(pan + math.pi / 3) < eps: #if within eps from full left, turn right
+        #        commands.setHeadPan(math.pi/3, time_delay)
+        #    print("=======================================")
+        #    print(pan)
+        #    print("=======================================")
+        #
 
 class Playing(LoopingStateMachine):
 
@@ -47,30 +69,9 @@ class Playing(LoopingStateMachine):
                 print(pan)
                 print("=======================================")
 
-    class TurnTowardBall(Node):
-        def run(self):
-            ball = mem_objects.world_objects[core.WO_BALL]
-            #commands.setStiffness()
-            if ball.seen:
-                commands.setHeadPan(ball.visionBearing, time_delay)
-                print("=======================================")
-                print(ball.visionBearing, ball.visionElevation)
-                print(ball.seen, ball.imageCenterX, ball.imageCenterY)
-                print("=======================================")
-            #else:
-            #    pan = core.joint_values[core.HeadPan]
-            #    if abs(pan - math.pi / 3) < eps: #if within eps from full right, turn left
-            #        commands.setHeadPan(-math.pi/3, time_delay)
-            #    elif abs(pan + math.pi / 3) < eps: #if within eps from full left, turn right
-            #        commands.setHeadPan(math.pi/3, time_delay)
-            #    print("=======================================")
-            #    print(pan)
-            #    print("=======================================")
-            #
-
     def setup(self):
-        #ball_turn = TurnTowardBall()
+        ball_turn = TurnTowardBall()
         scan = self.Scan()
 
-        self.add_transition(scan, T(time_delay), scan)
+        self.add_transition(scan, C, ball_turn, C, scan)
 
