dd77c2b29a44cb4b51bd1428f1b1e436d6a98503
Wally Guzman <gualbertoguzman@gmail.com>
2018-12-09 19:31:07 -0600

    Adding small data formatter script

---
+++ format_data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DATA="beacon_data.txt"
+FORMATTED="beacon_data_formatted.txt"
+SAVED="beacon_data.npz"
+
+# Copy data over
+scp nao@10.202.16.61:"/home/nao/beacon_data.txt" .
+
+# Format data in csv
+awk 'NR%15 {printf("%s, ", $0); next} {print $0}' $DATA > $FORMATTED
+
+# Save to npz
+python -c "import numpy as np; df = np.loadtxt(\"$FORMATTED\", delimiter=\",\"); np.savez(\"$SAVED\", actions=df[:,:3], observations=df[:,3:])"
